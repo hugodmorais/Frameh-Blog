2a45a3d913958de25bfdce1e9137b13321ba4456
Hugo Morais <hugodanielmorais@gmail.com>
2019-03-18 22:19:11 +0000

    Create table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_220923) do
+ActiveRecord::Schema.define(version: 2019_03_18_215432) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
